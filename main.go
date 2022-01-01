8efd43cab77232d48f42922b3c3523394598db04
samuelbartels20 <bartelssamuel20@gmail.com>
2022-01-01 19:36:18 +0000

    added comments

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
+// Declares what package this code is part of 
 package main
+
+/*
+Tells the compiler which packages i intend to use in my code
+*/
 import (
 	"fmt"
 	"net/http"
 )
 
+/* 
+This is the function i use to process incoming web requests
+*/
 func handlerFunc(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "<h1>This is the begining of my journey in Go programming</h1>")
 }
