a4c38bd0538d243a93199c7cf2009d2b9c191e37
samuelbartels20 <bartelssamuel20@gmail.com>
2022-01-02 23:05:04 +0000

    modified:   main.go

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
-package main 
+// Declares what package this code is part of 
+package main
 
-import "fmt"
+/*
+Tells the compiler which packages i intend to use in my code
+*/
+import (
+	"net/http"
+)
 
+/* 
+This is the function i use to process incoming web requests
+*/
+func handlerFunc(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to my awesome site!</h1>"))
+}
+
+// This is the function that will be run to start up your application
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3001", nil)
 }
 
